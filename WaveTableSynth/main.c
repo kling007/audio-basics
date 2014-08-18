3ea6653a72ad86d738d84b54a6793ee28830bc72
Garry Kling <garry.kling@gmail.com>
2014-08-17 21:53:22 -0700

    Initial Commit

---
+++ WaveTableSynth/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  WaveTableSynth
+//
+//  Created by Garry Kling on 8/17/14.
+//  Copyright (c) 2014 Garry Kling. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
